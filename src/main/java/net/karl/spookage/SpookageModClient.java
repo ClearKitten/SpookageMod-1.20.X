b704948d39decb43849f485ce879f218c6dfabb6
Karl Pettersson <karl.pettersson05@gmail.com>
2023-07-17 21:10:22 +0200

    Initial commit

---
+++ src/main/java/net/karl/spookage/SpookageModClient.java
@@ -0,0 +1,10 @@
+package net.karl.spookage;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpookageModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
